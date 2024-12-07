2e881ee7b33e1639b1082cd644d1ee1000c1d1ed
nemonousagi <122433427+ifindnemo@users.noreply.github.com>
2024-12-07 14:48:02 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import torch
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Tải mô hình đã huấn luyện
 model = torch.load('best.pt')
