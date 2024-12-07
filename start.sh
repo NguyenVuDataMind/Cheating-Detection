1935ab938fb2cbf54433c863cbcdaaa02a3768d9
nemonousagi <122433427+ifindnemo@users.noreply.github.com>
2024-12-07 22:00:03 +0700

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/app/libs
+
+# Cài đặt ultralytics mà không cài đặt các phụ thuộc của nó
+pip install ultralytics==8.0.0 --no-deps
+
 exec gunicorn server:app
