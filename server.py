8543d8516d440999fbd3a4a298c1f0e048a2c04e
nemonousagi <122433427+ifindnemo@users.noreply.github.com>
2024-12-08 10:31:19 +0700

    Add files via upload

--- server.py
+++ server.py
@@ -1,24 +1,36 @@
-from flask import Flask, request, jsonify
-import torch
+from flask import Flask, request
 from flask_cors import CORS
+from ultralytics import YOLO
+from PIL import Image
+import io
 
 app = Flask(__name__)
 CORS(app)
 
 # Tải mô hình đã huấn luyện
-model = torch.load('best.pt')
+model = YOLO('best.onnx')
+
+def read_image(file):
+    # Đọc ảnh từ file và chuyển đổi sang định dạng mà mô hình YOLO hỗ trợ
+    image = Image.open(io.BytesIO(file)).convert('RGB')
+    return image
 
 @app.route("/predict", methods=["POST"])
 def predict():
     # Nhận ảnh từ POST request
-    file = request.files['image']
-    img = file.read()
+    file = request.files['image'].read()
+    
+    # Đọc và chuyển đổi ảnh
+    image = read_image(file)
     
     # Chạy mô hình nhận diện ảnh
-    results = model(img)
-
-    # Trả về kết quả nhận diện dưới dạng JSON
-    return jsonify(results.pandas().xywh)
+    results = model(image)
+    for result in results:
+        print(result.boxes)  # Print detection boxes
+        #result.show()  # Display the annotated image
+        #result.save(filename="result.jpg")  # Save annotated imag
+    
+    return results[0].to_json()
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
