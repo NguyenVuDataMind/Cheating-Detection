b6efbf34b8942ec6d6048434ebfa1c35ca9e1c7f
nemonousagi <122433427+ifindnemo@users.noreply.github.com>
2024-12-08 11:39:33 +0700

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from ultralytics import YOLO
 from PIL import Image
@@ -7,30 +7,39 @@ import io
 app = Flask(__name__)
 CORS(app)
 
-# Tải mô hình đã huấn luyện
+# Load the trained model
 model = YOLO('best.onnx')
 
 def read_image(file):
-    # Đọc ảnh từ file và chuyển đổi sang định dạng mà mô hình YOLO hỗ trợ
+    # Read the image from file and convert it to a format supported by YOLO model
     image = Image.open(io.BytesIO(file)).convert('RGB')
     return image
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    # Nhận ảnh từ POST request
+    # Receive the image from the POST request
     file = request.files['image'].read()
     
-    # Đọc và chuyển đổi ảnh
+    # Read and convert the image
     image = read_image(file)
     
-    # Chạy mô hình nhận diện ảnh
+    # Run the model on the image
     results = model(image)
-    for result in results:
-        print(result.boxes)  # Print detection boxes
-        #result.show()  # Display the annotated image
-        #result.save(filename="result.jpg")  # Save annotated imag
     
-    return results[0].to_json()
+    # Extract detection results
+    detections = []
+    for box in results[0].boxes:
+        detection = {
+            "class_name": int(box.cls.item()),  # Assuming class names are integers
+            "confidence": float(box.conf.item()),
+            "x": float(box.xyxy[0][0].item()),
+            "y": float(box.xyxy[0][1].item()),
+            "width": float(box.xyxy[0][2].item() - box.xyxy[0][0].item()),
+            "height": float(box.xyxy[0][3].item() - box.xyxy[0][1].item())
+        }
+        detections.append(detection)
+    
+    return jsonify(detections)
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
