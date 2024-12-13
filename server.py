8e1ad138d6753860aef64c9b7fb81daa32225b9c
nemonousagi <122433427+ifindnemo@users.noreply.github.com>
2024-12-14 00:03:04 +0700

    Update server.py

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 cors = CORS(app)
 
 # Load the trained model
-model = YOLO('best.onnx')
+model = YOLO('best.pt')
 
 MONGO_URI = os.environ['MONGODB']
 client = MongoClient(MONGO_URI)
